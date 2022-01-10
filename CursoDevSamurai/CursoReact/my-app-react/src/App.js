23d56b2a35d0a6bbd0a096633b86ecc01f257ccb
Addson Costa <addson3001@gmail.com>
2022-01-10 09:40:22 -0300

    confirmar

---
+++ CursoDevSamurai/CursoReact/my-app-react/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const App = () =>
+<div class="div0-background">
+<h1>Currículo</h1>
+
+  <div class= "div1-central">
+
+
+  </div>
+
+</div>
+
+export default App;
---
+++ CursoFabApp/Criando Funcoes/script.js
@@ -0,0 +1,18 @@
+// função entrar
+function entrar() {
+  var area = document.getElementById('area');
+  var texto = prompt('Digite seu nome?');
+  if (texto == '' || texto == null) {
+    alert('Digite novamente!');
+    area.innerHTML = 'Bem vindo...';
+  }else{
+     area.innerHTML = 'Bem vindo, ' + texto;
+  }
+
+}
+
+function entrar2(nome) {
+  var area = document.getElementById('area2');
+  var texto = prompt('Digite seu sobrenome?');
+  area.innerHTML = nome + " " + texto;
+}
